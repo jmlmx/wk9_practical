ea9e2b9a5445744bc2d58523afd41aed2f423847
jmlmx <jmayon0@gmail.com>
2023-05-29 16:42:21 -0400

    Magic 8 Ball express application created.

--- server.js
+++ server.js
@@ -16,6 +16,13 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
     res.send(`Your tip of ${req.params.tipPercentage}% on a bill of $${req.params.total} is $${finalTip}.`)
 })
 
+app.get('/magic/:phrase', (req, res) => {
+    answerArray = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+    randomAnswer = answerArray[Math.floor(Math.random() * answerArray.length)]
+    res.send(`Your Question: ${req.params.phrase}?
+    <h1>${randomAnswer}</h1>`)
+})
+
 
 app.listen(port, () => {
     console.log(`The Port at ${port} is ready n' waiting...`)
