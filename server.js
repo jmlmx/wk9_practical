1b2e786450e8dad84aab39e25ae05ca09fc16ddb
jmlmx <jmayon0@gmail.com>
2023-05-29 20:29:55 -0400

    Fibonacci.

--- server.js
+++ server.js
@@ -23,6 +23,17 @@ app.get('/magic/:phrase', (req, res) => {
     <h1>${randomAnswer}</h1>`)
 })
 
+//Fibonacci - Hope this is right
+app.get('/fibonacci/:num', (req, res) => {
+    const fibSequence = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765,10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229, 832040, 1346269, 2178309, 3524578, 5702887, 9227465, 14930352, 24157817, 39088169, 63245986, 102334155]
+    const numInt = Number(req.params.num)
+    if (fibSequence.includes(numInt)) {
+        res.send("Very good. It is Fibonacci.")
+    }else {
+        res.send('I can tell this is not a fibonacci number.')
+    }
+})
+
 
 app.listen(port, () => {
     console.log(`The Port at ${port} is ready n' waiting...`)
